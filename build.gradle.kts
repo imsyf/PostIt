78233791f63450f97db3262cdcc39b82a0e5a9f7
imsyf <alghozalyi@gmail.com>
2022-11-09 07:12:37 +0700

    Set up dependency versions manager

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.diffplug.gradle.spotless.SpotlessExtensionPredeclare
 
 buildscript {
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+        classpath(AndroidX.navigation.safeArgsGradlePlugin)
     }
 }
 
@@ -23,7 +23,7 @@ subprojects {
             kotlin {
                 target("**/*.kt")
                 targetExclude("$buildDir/**/*.kt", "bin/**/*.kt")
-                ktlint("0.47.1")
+                ktlint(libs.versions.ktlint.get())
                     /**
                      * Spotless does not (yet) respect the `.editorconfig` file, see:
                      * https://github.com/diffplug/spotless/issues/142
@@ -54,6 +54,6 @@ configure<SpotlessExtension> {
 
 configure<SpotlessExtensionPredeclare> {
     kotlin {
-        ktlint("0.47.1")
+        ktlint(libs.versions.ktlint.get())
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,10 @@ pluginManagement {
         mavenCentral()
     }
 }
+plugins {
+    // Refer to https://github.com/jmfayard/refreshVersions/releases
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
